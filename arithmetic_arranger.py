9873cd983b4a3aa3e17efaa1fe34909b62452def
Soore Akintelure <103961262+oakintel@users.noreply.github.com>
2022-06-16 16:18:07 +0000

    Updating the idea behind the project approach.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,2 +1,16 @@
+import time
 
-arithmetic_arranger = list()
+
+def arithmetic_arranger():
+  print('Hello, and welcome'); time.sleep(0)
+  print('Please, insert problems in "x+y" format'); time.sleep(0)
+  print('Add multiple problems via ["x+y", "a+b"]'); time.sleep(0)
+    
+  ar = []
+  
+  data = input('Insert problems now: ')
+  ar.append(data)
+  print(ar)
+
+
+arithmetic_arranger()
