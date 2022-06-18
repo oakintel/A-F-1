94b78c93ac28eed62f4c83614d4728f9e5759104
Soore Akintelure <103961262+oakintel@users.noreply.github.com>
2022-06-18 02:20:34 +0000

    Debugged and Finalised the program.

--- main.py
+++ main.py
@@ -1,62 +1,63 @@
 import re
 
-sum = ()
-length = ()
-numerator = ()
-denominator = ()
-
 def arithmetic_arranger(problems, status = False):
-   
+
     if (len(problems) > 5):
       return 'Error: Too many problems.'
     
+    first = ""
+    second = ""
+    lines = ""
+    tots = ""
+    completion = ""
+  
+    
+    
     for problem in problems:
-     if re.search("[^\s0-9.+-]", problem):
-       if re.search("[/]") or re.search("[*]"):
-         return('Error: Operator must be '+' or '-'.')
-       return('Error: Number must only contain digits.')
+     if (re.search("[^\s0-9.+-]", problem)):
+       if (re.search("[/]", problem) or re.search("[*]", problem)):
+         return "Error: Operator must be '+' or '-'."
+       return "Error: Numbers must only contain digits."
 
      valueone = problem.split(" ")[0]
      valuetwo = problem.split(" ")[2]
      operator = problem.split(" ")[1]
 
-     if (len(valueone) or len(valuetwo)) > 4:
-       return 'Error: Numbers cannot be more than four digits'
+     if (len(valueone)>= 5 or len(valuetwo) >= 5):
+       return "Error: Numbers cannot be more than four digits."
 
-     sum = ("")
-     if operator == ("+"):
+     value = ""
+     if (operator == "+"):
        value = str(int(valueone) + int(valuetwo))
-     elif operator == ("-"):
+     elif (operator == "-"):
        value = str(int(valueone) - int(valuetwo))
        
-     length = max(len(valueone), len(valuetwo))
-     #numerator = valueone.rjust(length)
-     #denominator = operator + valuetwo.rjust(length - 1)
-     #result = str(sum).rjust(length)
-     #line = ()
-    
-    numerator += valueone.rjust(length + 2)
-    denominator = operator + ' ' + valuetwo.rjust(space)
-    divline = '-' * (length + 2)
+     length = max(len(valueone), len(valuetwo)) + 2
+     numerator = str(valueone).rjust(length)
+     denominator = operator + str(valuetwo).rjust(length - 1)
+     result = str(value).rjust(length)
+     line = ""
+			
+     for s in range(length):
+      line += "-"
 
+			
+     if  problem != problems[-1]:
+       	first += numerator + '    '
+       	second += denominator + '    '
+       	lines += line + '    '
+       	tots += result + '    '
+     else:
+        first += numerator 
+        second += denominator
+        lines += line
+        tots += result
 
-      
-     
-     
-      
-       
-     
+    if status == True:
+      completion = first + "\n" + second + "\n" + lines + "\n" + tots
+    elif status == False: 
+      completion = first + "\n" + second + "\n" + lines
+    return completion
+				
 
-       
-  
-#        one = problem.split(" ")[0]
-#       formula = problem.split(" ")[1]
-#        two = problem.split(" ")[2]
-#        if len(one) or len(two) > 4:
-#          return ('Error: Numbers cannot be more than four digits.')
-
-
-#arithmetic_arranger()
-
-print('Love you forever')
-#print(arithmetic_arranger(["32 + 698","3801 - 2", "45 + 43", "123+49"]))
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], False))
